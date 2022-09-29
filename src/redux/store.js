05ae8454708221670d9b5e90a190ed597515eefb
HailMuffins <rafaelsm123@outlook.com>
2022-09-29 14:54:11 -0300

    Configuração Redux Parcial

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,16 @@
 // configure aqui sua store
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/core';
+import { thunk } from 'redux-thunk';
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
+
+if (window.Cypress) {
+  window.store = store;
+}
