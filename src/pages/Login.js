e3feb2a31b626e3a03b4bd6d5fc4089bac08e6ca
HailMuffins <rafaelsm123@outlook.com>
2022-09-29 14:54:27 -0300

    Início Requisito 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,44 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  }
+
+  handleValidation = ({ target }) => {};
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor='user-email'>
+            <input
+              type='email'
+              name='user-email'
+              id='user_email'
+              data-testid='email-input'
+              onChange={this.handleValidation}
+            />
+          </label>
+          <label htmlFor='user_password'>
+            <input
+              type='text'
+              name='user_password'
+              id='user_password'
+              data-testid='password-input'
+              onChange={this.handleValidation}
+            />
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
