7c187e72d91e2f4f369b4c77aa92fd75a4201b80
HailMuffins <rafaelsm123@outlook.com>
2022-10-02 12:22:48 -0300

    Requisito 2 Completado Criado Login.css Criado Header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Header.css";
 
 class Header extends Component {
   render() {
+    console.log(this.props);
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <div className="banner">TRYBE WALLET</div>
+        <div className="userInfo">
+          <div data-testid='email-field'>Email: {email}</div>
+          <div data-testid='total-field'>Gastos: 0</div>
+          <div data-testid='header-currency-field'>Câmbio: BRL</div>
+        </div>
+      </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({ email: state.user.email });
+
+export default connect(mapStateToProps, null)(Header);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { actSubmitUserEmail } from "../redux/actions";
+import './Login.css';
 
 class Login extends React.Component {
   constructor(props) {
